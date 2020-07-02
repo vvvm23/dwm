b9d5f318379bf768bc5d0363e3429a97242ca90c
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-07-02 14:28:51 +0100

    floating windows now open in center (why isnt this default? :thinking: )

--- dwm.c
+++ dwm.c
@@ -1146,6 +1146,11 @@ manage(Window w, XWindowAttributes *wa)
 	}
 
 	wc.border_width = c->bw;
+
+  /* open float in center */
+  if (c->x == selmon->wx) c->x += (c->mon->ww - WIDTH(c)) / 2 - c->bw;
+  if (c->y == selmon->wy) c->y += (c->mon->wh - HEIGHT(c)) / 2 - c->bw;
+
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
